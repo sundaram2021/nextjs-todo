56c1beeb229c992e2d242d43bfdb0bf54a2a0944
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-05-09 23:27:24 +0530

    added contact route

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+interface IProps {};
+
+const Contact:FC<IProps> = (props) => {
+    return <div>
+        <h1>Contact
+            
+        </h1>
+    </div>
+};
+
+export default Contact;
