fcb8646f7d731372b0975cdf2aac772c3c5843fe
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-05-09 23:27:52 +0530

    started nextjs 13.4

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+interface IProps {};
+
+const About:FC<IProps> = (props) => {
+    return <div>
+        <h1>ABout</h1>
+    </div>
+};
+
+export default About;
