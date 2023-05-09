96a4c36444991023c9b15879c4c9d68c1c3f1f90
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-05-09 23:27:07 +0530

    added page/23 route

---
+++ src/app/auth/[login]/page.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface IProps {};
+
+const Login:FC<IProps> = (props) => {
+    return <div><h1>Dynamic Route</h1></div>
+};
+
+export default Login;
---
+++ src/app/auth/page.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface IProps {};
+
+const componentName:FC<IProps> = (props) => {
+    return <div><h1>Auth</h1></div>
+};
+
+export default componentName;
