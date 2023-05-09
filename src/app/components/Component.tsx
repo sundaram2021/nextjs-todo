a18e260460bea59d08ff05aa91a77d01020d3d43
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-05-09 23:26:27 +0530

    component inside the folder of app without page.tsx file init can be used for other puposes like components and other things

---
+++ src/app/components/Component.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+
+interface IProps {};
+
+const Componenent:FC<IProps> = (props) => {
+    return <div><h1>Component</h1>
+    </div>
+};
+
+export default Componenent;
